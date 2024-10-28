f3e1044ad02bf93224224922c86423442828e6c9
BenjaminS04 <benscanlon04@gmail.com>
2024-10-28 09:09:47 +0000

    updated iam instance  profile to use new profiles.

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,28 +3,27 @@ resource "aws_instance" "ec2" {
   ami                  = var.ami
   instance_type        = var.instance_type
   key_name             = var.key_name
-  iam_instance_profile = "EC2InstanceProfile"
+  iam_instance_profile = "${var.each_key}-EC2InstanceProfile"
    #user data used to run script upon ec2 initialisation
 
   user_data = <<-EOF
-              #!/bin/bash
+    #!/bin/bash
 
-              
-              # gets cloudwatch logging dependancies
+    
+    # gets cloudwatch logging dependancies
 
-              wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-              dpkg -i -E ./amazon-cloudwatch-agent.deb
-              
-              
-              
-              
-              
+    wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+    dpkg -i -E ./amazon-cloudwatch-agent.deb
+    
+    
+    # unique instance user data
 
-              ${var.additional_user_data}
-              EOF
+    ${var.additional_user_data} 
+    EOF
   
   lifecycle {
-    create_before_destroy = true # makes sure old resource stays live until new one is created to minimize down time
+    # makes sure old resource stays live until new one is created to minimize down time
+    create_before_destroy = true
   }
 
 
