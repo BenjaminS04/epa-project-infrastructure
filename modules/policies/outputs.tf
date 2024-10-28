a79eadedb1ddd0441622ebe06e7dd35ecc820cd8
BenjaminS04 <benscanlon04@gmail.com>
2024-10-28 11:42:53 +0000

    policy output naming fix

--- modules/policies/outputs.tf
+++ modules/policies/outputs.tf
@@ -1,7 +1,7 @@
-output "target-iam_instance_profile" {
+output "target_iam_instance_profile" {
   value = aws_iam_instance_profile.target_ec2_instance_profile
 }
 
-output "monitor-iam_instance_profile" {
+output "monitor_iam_instance_profile" {
   value = aws_iam_instance_profile.monitor_ec2_instance_profile
 }
