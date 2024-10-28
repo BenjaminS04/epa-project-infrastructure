719438138e9056c2adaa94b9e801aefca8cfb0e7
BenjaminS04 <benscanlon04@gmail.com>
2024-10-28 11:35:38 +0000

    fixed output file for policies, now outputs both profiles.

--- modules/policies/outputs.tf
+++ modules/policies/outputs.tf
@@ -1,3 +1,7 @@
 output "target-iam_instance_profile" {
-  value = aws_iam_instance_profile.ec2_instance_profile.name
+  value = aws_iam_instance_profile.target_ec2_instance_profile
+}
+
+output "monitor-iam_instance_profile" {
+  value = aws_iam_instance_profile.monitor_ec2_instance_profile
 }
