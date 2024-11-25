38a3075906ca286c00ff6237c477c81820c56d44
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-11-25 15:19:51 +0000

    http ingress removed as it should no longer be necessary

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -5,12 +5,12 @@ resource "aws_security_group" "sg" {
   name        = var.security_group_name
   description = "ec2 sg"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     from_port   = 443
