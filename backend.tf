a6fd89ea65442017ada2f844c58955e81ab3e0a5
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-12-16 16:16:24 +0000

    started adding remote state

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "epa-backend-terraform-remote-states"  # Replace with your S3 bucket name
+    key            = "/${var.environment}-terraform state"     # Path within the bucket to store the state file
+    region         = var.region                    # AWS region of the bucket
+    encrypt        = true                            # Encrypt state file at rest
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
