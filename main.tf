838f9ea013066fe0851126952c85c55653aecb97
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-12-30 13:46:49 +0000

    backend added to main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ terraform { #sets required providers and versions
       version = "5.72.1"
     }
   }
+
+  backend "s3" {
+    bucket  = "epa-backend-terraform-remote-states" # Replace with your S3 bucket name
+    key     = "/${var.environment}-terraform state" # Path within the bucket to store the state file
+    region  = var.region                            # AWS region of the bucket
+    encrypt = true                                  # Encrypt state file at rest
+  }
 }
 
 provider "aws" {
