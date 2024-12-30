c3f2a9ec92b11bf4910b1811c2f73c75a251d594
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-12-30 15:17:55 +0000

    removed unecessary extra s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -36,25 +36,7 @@ resource "aws_s3_bucket_public_access_block" "private" {
 
 }
 
-resource "aws_s3_bucket_public_access_block" "state_private" {
 
-  bucket   = aws_s3_bucket.terraform_state.id
-  
-
-  # Blocks the use of public ACLs on this S3 bucket.
-  block_public_acls = true
-
-  # Prevents the bucket from having public bucket policies.
-  block_public_policy = true
-
-  # Ignores any public ACLs on the bucket's objects.
-  ignore_public_acls = true
-
-  # Restricts public bucket policies from allowing cross-account access unless explicitly authorized.
-  restrict_public_buckets = true
-
-
-}
 
 # versioning for s3 buckets, means changes are reversable.
 resource "aws_s3_bucket_versioning" "s3_versioning" {
@@ -71,44 +53,7 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
 
 }
 
-# versioning for s3 buckets, means changes are reversable.
-resource "aws_s3_bucket_versioning" "s3_state_versioning" {
-  
-  bucket   = aws_s3_bucket.terraform_state.id
-  
-
-  versioning_configuration {
-    status = "Enabled"
-
-    # # prevents deletion without using mfa (multi-factor authenication)
-    # mfa_delete = "Enabled"
-  }
-
-}
-
-
-//---------- terraform state bucket ----------
-
-
-resource "aws_s3_bucket" "terraform_state" {
-  
-  bucket = "epa-backend-terraform-remote-states"
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
 
 
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
